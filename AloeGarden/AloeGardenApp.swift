be3318884dad31e19a7ba7758b4aac1506195ffd
Tsuskov <159012019+Tsuskov@users.noreply.github.com>
2026-06-17 18:13:24 +0200

    Initial Commit

---
+++ AloeGarden/AloeGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AloeGardenApp.swift
+//  AloeGarden
+//
+//  Created by Tim Suskov on 17.06.26.
+//
+
+import SwiftUI
+
+@main
+struct AloeGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
